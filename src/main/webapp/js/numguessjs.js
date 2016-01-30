950d4e2ee5e46f47b8843c78008b7158d9ccdb68
Karczag Zoltan <zoltankarczag77@gmail.com>
2016-01-30 14:59:42 +0200

    css

--- src/main/webapp/js/numguessjs.js
+++ src/main/webapp/js/numguessjs.js
@@ -55,7 +55,7 @@
                  else
                  if(keySuccess=="true")
                  {
-                     document.getElementById("serverResponse").innerHTML = "Congrats, you guessed the number " + document.getElementById("number").value + " after " + keyNrGuesses + " guesses. " + "TIME: " + totalTime + " " + timeMin + "";
+                     document.getElementById("serverResponse").innerHTML = "Congrats, you guessed the number " + document.getElementById("number").value + " after " + keyNrGuesses + " guesses. " + " " + totalTime + " CURRENT TIME: " + timeMin + "";
 
                  }
              }
