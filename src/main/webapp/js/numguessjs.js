a2ef52efe9a78fe632a53bbff3ee9bf40116595d
Karczag Zoltan <zoltankarczag77@gmail.com>
2016-02-02 11:56:28 +0200

    new look

--- src/main/webapp/js/numguessjs.js
+++ src/main/webapp/js/numguessjs.js
@@ -1,6 +1,6 @@
        function reset(){
             document.getElementById("serverResponse").innerText="";
-            xmlHttp = new XMLHttpRequest();
+            var xmlHttp = new XMLHttpRequest();
             xmlHttp.onreadystatechange=callback;
             var url ="NumGenServlet"+"?requestRestartGame=1";
             xmlHttp.open("GET",url,true);
